d35ab2495ae415e8de39414618d3c9889c4221c7
VasyaStelmah <stelmah.v.m@gmail.com>
2021-06-21 10:45:00 +0300

    42 task

--- pdp/TASKS/42_array_filter.js
+++ pdp/TASKS/42_array_filter.js
@@ -1,10 +1,6 @@
 //Create a function that takes an array of non-negative integers and strings and return a new array without the strings.
-filterArray = (array) => {
-    return array.filter(item => {
-        return item > 0 && typeof item === 'number'
-    })
-}
+filterArray = (array) => array.filter(item => typeof item === 'number')
+
 //Examples
 console.log(filterArray([1, 2, "a", "b"])) //➞ [1, 2]
 console.log(filterArray([1, "a", "b", 0, 15]))// ➞ [1, 0, 15]
-console.log(filterArray([1, 2, "aasf", "1", "123", 123])) //➞ [1, 2, 123]
