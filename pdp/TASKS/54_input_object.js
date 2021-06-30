8fbac2a7221fa879d1cff7db40d66408aa28f734
VasyaStelmah <stelmah.v.m@gmail.com>
2021-06-30 17:12:19 +0300

    fixed 54 task

--- pdp/TASKS/54_input_object.js
+++ pdp/TASKS/54_input_object.js
@@ -4,22 +4,22 @@ let inputData = [{
 	lawArticle: "12.09.2 - Превышение скорости движения ТС от 20 до 40 км/ч",
 	documentTitle:
 		"Постановление по видеофиксации №11111111111111111111 от 20.05.2021",
+	coordinates: {
+		lat: '55.7522200',
+		long: '37.6155600'
+	},
+	coordinate: ['55.7522200', '37.6155600'],
 }];
-const root = document.getElementById("root")
-inputDataFunc = (data) => {
-	data.map((item) => {
-		root.insertAdjacentHTML(
-			"afterbegin",
-			`<div>Место</div>
-			<div>${item.place}</div>
-			<div>Дата</div>
-			<div>${item.date}</div>
-			<div>Статья</div>
-			<div>${item.lawArticle}</div>
-			<div>Постановление</div>
-			<div>${item.documentTitle}</div>
-			`
-		);
+insertElement = (key, value) => root.insertAdjacentHTML("beforeend", `<h3>${key}:</h3><div>${typeof value === 'string' ? value : Object.values(value)}</div>`);
+inputDataFunc = (array) => {
+	return array.map((obj) => {
+		return Object.entries(obj).map((item) => {
+			item[0] === 'place' ? insertElement('Место', item[1])
+				: item[0] === 'date' ? insertElement('Дата', item[1])
+					: item[0] === 'lawArticle' ? insertElement('Статья', item[1])
+						: item[0] === 'documentTitle' ? insertElement('Постановление', item[1])
+							: insertElement(item[0], item[1])
+		})
 	})
 };
-inputDataFunc(inputData)
+inputDataFunc(inputData);
