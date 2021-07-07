d5a9a1356ec5d1987b39ec16020f42099fd9db89
VasyaStelmah <stelmah.v.m@gmail.com>
2021-07-07 10:23:48 +0300

    #62 Вынести хедер, отдельным модулем

--- react-app/client/src/Footer.jsx
+++ react-app/client/src/Footer.jsx
@@ -1,4 +1,4 @@
-import './LendingPage.css';
+import './footer.css';
 export default function Footer() {
 	return (
 		<footer className="footer-outer">
--- react-app/client/src/LendingPage.jsx
+++ react-app/client/src/LendingPage.jsx
@@ -3,35 +3,6 @@ import Footer from './Footer';
 export default function App() {
   return (
     <>
-      <header className="showcase">
-        <div className="showcase-top">
-          <img alt="Netflix" src="https://i.ibb.co/r5krrdz/logo.png" />
-          <div className="showcase-top__right">
-            <div className="showcase-top__right-lang">
-              <select>
-                <option value="/by/" lang="en">English</option>
-                <option value="/by-ru/" lang="ru">Русский</option>
-              </select>
-            </div>
-            <div className="showcase-top__right-signin">
-              <a href="#" className="btn btn-rounded">Sign In</a>
-            </div>
-          </div>
-        </div>
-        <div className="showcase-content">
-          <h1 className="showcase-content__title">Unlimited movies, TV shows, and more.</h1>
-          <div className="showcase-content__subtitle">Watch anywhere. Cancel anytime.</div>
-          <div className="showcase-content__text">Ready to watch? Enter your email to create or restart your membership.</div>
-          <div className="showcase-content__form">
-            <div className="showcase-content__form-input">
-              <input type="text" placeholder="Email adress" />
-            </div>
-            <div className="showcase-content__form-button">
-              <button>Get started</button>
-            </div>
-          </div>
-        </div>
-      </header>
       <section className="our-story-card-outer">
         <div className="our-story-card-inner">
           <div className="our-story-card__text">
