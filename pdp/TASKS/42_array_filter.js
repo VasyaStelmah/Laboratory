fde68a54db1f17b6ed5ef0d3328781c4e1d45740
VasyaStelmah <stelmah.v.m@gmail.com>
2021-07-07 09:12:17 +0300

    42,44 task

--- pdp/TASKS/42_array_filter.js
+++ pdp/TASKS/42_array_filter.js
@@ -1,9 +1,5 @@
 //Create a function that takes an array of non-negative integers and strings and return a new array without the strings.
 filterArray = (array) => array.filter(item => typeof item === 'number')
-<<<<<<< HEAD
-
-=======
->>>>>>> 42_array_filter
 //Examples
 console.log(filterArray([1, 2, "a", "b"])) //➞ [1, 2]
 console.log(filterArray([1, "a", "b", 0, 15]))// ➞ [1, 0, 15]
--- pdp/TASKS/44_reverse_case.js
+++ pdp/TASKS/44_reverse_case.js
@@ -1,21 +1,22 @@
 //Given a string, create a function to reverse the case. All lower-cased letters should be upper-cased, and vice versa.
-
 reverseCase = (string) => {
-    let newString = "";
-    for (let i = 0; i < string.length; i++) {
-        if (string[i] === ' ') {
-            newString = newString + string[i]
-        }
-        if (string[i].match(/[A-Z]/)) {
-            newString = newString + string[i].toLowerCase()
-        }
-        if (string[i].match(/[a-z]/)) {
-            newString = newString + string[i].toUpperCase()
-        }
-    }
-    return newString;
+	const newString = "";
+	for (let i = 0; i < string.length; i++) {
+		if (string[i].match(/[A-Z]/)) {
+			let lowerCase = string[i].toLowerCase();
+			console.log(lowerCase);
+			newString.concat(lowerCase);
+		}
+		if (string[i].match(/[a-z]/)) {
+			let upperCase = string[i].toUpperCase();
+			console.log(upperCase);
+			newString.concat(upperCase);
+		}
+	}
+	return newString;
 }
 //Examples
 console.log(reverseCase("Happy Birthday")) //➞ "hAPPY bIRTHDAY"
 console.log(reverseCase("MANY THANKS")) //➞ "many thanks"
 console.log(reverseCase("sPoNtAnEoUs")) //➞ "SpOnTaNeOuS"
+console.log("".concat('fdfdfdf'))
