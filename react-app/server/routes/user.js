d9663824ab7986c7c388da994b32c7ecef1a0886
VasyaStelmah <stelmah.v.m@gmail.com>
2021-07-16 16:31:08 +0300

    changed the file directory react-app

--- react-app/server/routes/user.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const controller = require("../controllers/user.js");
-
-router.post("/signin", controller.signin);
-router.post("/signup", controller.signup);
-
-module.exports = router;
