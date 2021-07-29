4bda7495d4a41b34a1402d2228016b32fb1743d7
VasyaStelmah <stelmah.v.m@gmail.com>
2021-07-29 19:33:56 +0300

    #66 fixed mistakes

--- src/components/SectionFAQ/SectionFAQ.tsx
+++ src/components/SectionFAQ/SectionFAQ.tsx
@@ -1,41 +1,38 @@
 import './SectionFAQ.scss';
+const linkText = [
+  'What is Netflix?',
+  'How much does Netflix cost?',
+  'Where can I watch?',
+  'How do I cancel?',
+  'What can I watch on Netflix?',
+  'Is Netflix good for kids?',
+  'Why am I seeing this Language?'
+]
 
 export default function SectionFAQ() {
-	  return (
-      <section className="faq-question-outer">
-        <div className="faq-question-inner">
-          <div className="faq-question__text-title">Frequently Asked Questions</div>
-          <ul className="faq-question__button">
-            <a href="#section">
-              <h2>What is Netflix?</h2>
-            </a>
-            <a href="#section">
-              <h2>How much does Netflix cost?</h2>
-            </a>
-            <a href="#section">
-              <h2>Where can I watch?</h2>
-            </a>
-            <a href="#section">
-              <h2>How do I cancel?</h2>
-            </a>
-            <a href="#section">
-              <h2>What can I watch on Netflix?</h2>
-            </a>
-            <a href="#section">
-              <h2>Is Netflix good for kids?</h2>
-            </a>
-            <a href="#section">
-              <h2>Why am I seeing this Language?</h2>
-            </a>
-          </ul>
-          <h2 className="faq-question-subtitle">Ready to watch? Enter your email to create or restart your membership.</h2>
-          <div className="faq-question__form">
-            <div className="faq-question__form-input">
-              <input type="text" placeholder="Email adress" />
-            </div>
-            <div className="faq-question__form-button"><button>Get started</button></div>
+	return (
+    <section className="faq-question-outer">
+      <div className="faq-question-inner">
+        <div className="faq-question__text-title">Frequently Asked Questions</div>
+        <ul className="faq-question__button">
+          {linkText.map((item,index)=>{
+            return (
+              <a href="#section" key={index}>
+                <h2>{item}</h2>
+              </a>
+            )
+          })}
+        </ul>
+        <h2 className="faq-question-subtitle">Ready to watch? Enter your email to create or restart your membership.</h2>
+        <div className="faq-question__form">
+          <div className="faq-question__form-input">
+            <input type="text" placeholder="Email adress" />
+          </div>
+          <div className="faq-question__form-button">
+            <button>Get started</button>
           </div>
         </div>
-      </section>
-		)
+      </div>
+    </section>
+	)
 }
