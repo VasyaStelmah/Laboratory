1823ad8fc0cca1498f590bb5882210b2eb0d6573
VasyaStelmah <stelmah.v.m@gmail.com>
2021-08-12 18:51:29 +0300

    add typescript tasks

---
+++ pdp/TYPESCRIPT_TASKS/3.ts
@@ -0,0 +1,11 @@
+/* eslint-disable no-console */
+// Your task is to remove all duplicate words from a string, leaving only single (first) words entries.
+// Input:
+// 'alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta'
+// Output:
+// 'alpha beta gamma delta'
+function removeDuplicateWords(s: string): string {
+  return [...new Set(s.split(' '))].join(' ');
+}
+console.log(removeDuplicateWords('alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta'));
+// 'alpha beta gamma delta'
