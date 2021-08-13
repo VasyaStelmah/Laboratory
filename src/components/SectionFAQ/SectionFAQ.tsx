693cea294bb718a852f19c0a829f96c94eebdb0f
VasyaStelmah <stelmah.v.m@gmail.com>
2021-08-13 07:58:04 +0300

    shared files with data and markup tsx

--- src/components/SectionFAQ/SectionFAQ.tsx
+++ src/components/SectionFAQ/SectionFAQ.tsx
@@ -1,14 +1,5 @@
 import './SectionFAQ.scss';
-
-const linkText = [
-  { text:'What is Netflix?', id: 1 },
-  { text:'How much does Netflix cost?', id: 2 },
-  { text:'Where can I watch?', id: 3 },
-  { text:'How do I cancel?', id: 4 },
-  { text:'What can I watch on Netflix?', id: 5 },
-  { text:'Is Netflix good for kids?', id: 6 },
-  { text:'Why am I seeing this Language?', id: 7 }
-];
+import { linkText } from './content';
 
 export default function SectionFAQ() {
 	return (
--- src/pages/Main/LendingPage.tsx
+++ src/pages/Main/LendingPage.tsx
@@ -6,38 +6,9 @@ import SectionCard from '../../components/SectionCard';
 import SectionCardText from '../../components/SectionCard/SectionCardText';
 import SectionCardVideo from '../../components/SectionCard/SectionCardVideo';
 import SectionFAQ from '../../components/SectionFAQ';
+import { sectionCard } from './content';
 
 export default function LendingPage() {
-  const sectionCard = [
-    {
-      id:1,
-      title:'Enjoy on your TV.',
-      subtitle:'Watch on Smart TVs, Playstation, Xbox, Chromecast, Apple TV, Blu-ray players, and more.',
-      alt:"Watch on TV, PS, Xbox",
-      src:"https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/tv.png",
-    },
-    {
-      id:2,
-      title:'Download your shows to watch offline.',
-      subtitle:'Save your favorites easily and always have something to watch',
-      alt:"watch offline",
-      src:"https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/mobile-0819.jpg",
-    },
-    {
-      id:3,
-      title:'Watch everywhere.',
-      subtitle:'Stream unlimited movies and TV shows on your phone, tablet, laptop, and TV without paying more.',
-      alt:"Stream unlimited movies",
-      src:"https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/device-pile.png",
-    },
-    {
-      id:4,
-      title:'Create profiles for kids.',
-      subtitle:'Send kids on adventures with their favorite characters in a space made just for them—free with your membership.',
-      alt:"profiles for kids",
-      src:'https://occ-0-4923-2774.1.nflxso.net/dnm/api/v6/19OhWN2dO19C9txTON9tvTFtefw/AAAABdFTpLmANuJpYneLq8L5m7CunMCi8e8Nl4y7xaPVWzG3IeoDoq17egTQAthApKg_4sdRWdwuR8KadWu1frjL3JQImpwq.png?r=fcd',
-    },
-  ];
   const [isModal, setIsModal]= useState<boolean>(false);
   return (
     <>
