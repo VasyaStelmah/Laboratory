73ee69c0e7d37c5194edb6f86044a35f6e74e614
VasyaStelmah <31704350+VasyaStelmah@users.noreply.github.com>
2021-08-13 22:09:38 +0300

    Update Header.tsx

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,11 +6,11 @@ import Footer from '../Footer/Footer';
 import { useActions } from "../../hooks/useActions";
 import { useTypedSelector } from "../../hooks/useTypedSelector";
 
-type Propss={
+type Props={
 	isModal:boolean,
 	setIsModal:React.Dispatch<React.SetStateAction<boolean>>
 };
-export default function Header({ isModal, setIsModal }:Propss) {
+export default function Header({ isModal, setIsModal }:Props) {
   const { user, loading, error } = useTypedSelector(state => state.auth);
   const { signin, signup } = useActions();
 
